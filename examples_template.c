c01be2757b46897afbe7e169d19db2597626aa8a
TitasBSP <titasbsp@gmail.com>
2025-05-22 12:45:14 +0200

    login start

--- examples_template.c
+++ examples_template.c
@@ -15,6 +15,7 @@
 // AND MUCH MORE!
 
 
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -32,30 +33,60 @@ int main(void)
     SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
     ToggleFullscreen();
     
+    // Font
+    Font JupiterFont = LoadFont("romulus.png");
+
+    // Images to initialize
+    Image loginScreenPNG = LoadImage("mainmenucover.png");  
+    Texture2D textureLogin = LoadTextureFromImage(loginScreenPNG);  
+    UnloadImage(loginScreenPNG);
+    
+    Image loginLogoPNG = LoadImage("canoeLogo.png");
+    ImageResize(&loginLogoPNG, 256, 256);
+    Texture2D textureLoginLogo = LoadTextureFromImage(loginLogoPNG);  
+    
+    // Booleans for screens
+    bool loginScreen = true;
+    
+    // Positions for vectors
+    Vector2 posCanoeLogin = {850, 200};
+    
+    // Alphas
+    float alphaLogin = 0.0f;   
+    float alphaLoginLogo = 0.0f;
+    
     //--------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update variables / Implement example logic at this point
-        //----------------------------------------------------------------------------------
-
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            // TODO: Draw everything that requires to be drawn at this point:
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);  // Example
+        
+            if (loginScreen) {
+                if (alphaLogin < 1) alphaLogin += 0.03f;
+                if (alphaLoginLogo < 0.8) alphaLoginLogo += 0.03f;
+                ClearBackground(BLACK);
+                DrawTexture(textureLogin, screenWidth-800, screenHeight-900, Fade(WHITE, 0.5f)); 
+                DrawTextEx(JupiterFont, "CANOE", posCanoeLogin, 150, 25, Fade(WHITE, alphaLogin));  
+                DrawTexture(textureLoginLogo, screenWidth-250, screenHeight-265, Fade(WHITE, alphaLoginLogo));
+                
+                DrawRectangle(640, 510, 620, 120, Fade(BLACK, alphaLogin));
+                DrawRectangle(650, 520, 600, 100, Fade(DARKGRAY, alphaLogin));                
+                
+                DrawRectangle(640, 670, 620, 120, Fade(BLACK, alphaLogin));
+                DrawRectangle(650, 680, 600, 100, Fade(DARKGRAY, alphaLogin));
+            }
+            
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
-
+    
+    UnloadImage(loginLogoPNG);
+    UnloadTexture(textureLoginLogo);
+ 
     // De-Initialization
     //--------------------------------------------------------------------------------------
 
